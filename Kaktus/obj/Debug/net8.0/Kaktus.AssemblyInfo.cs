bbbc868facb9f429ad2e4cf94a153bffc17f13f5
rawer470 <timont60@bk.ru>
2024-11-08 23:09:22 +0300

    FixBugCollapse2

--- Kaktus/obj/Debug/net8.0/Kaktus.AssemblyInfo.cs
+++ Kaktus/obj/Debug/net8.0/Kaktus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kaktus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86cad7e2cdd4b3f50074334b8fb1d8be70ef710c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f118444514ead12423d1fcfb01c709ae800ca20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kaktus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kaktus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
